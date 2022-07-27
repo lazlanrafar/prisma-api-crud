83a123c9dbf2ab3b7eb616f91262bbc33d7faa1c
lazlanrafar <lazlanrafar@gmail.com>
2022-07-27 22:06:30 +0700

    setup port

--- app.js
+++ app.js
@@ -39,4 +39,9 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
 module.exports = app;
