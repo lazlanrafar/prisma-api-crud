3e1029283b42fecf7628ca662f6cac842d21a942
lazlanrafar <lazlanrafar@gmail.com>
2022-07-27 22:33:27 +0700

    setup routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/products", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/products/:id", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/products", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.patch("/products/:id", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.delete("/products/:id", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
