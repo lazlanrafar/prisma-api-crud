adde79eb3bba500fbd7b9ce473ed35d757ac30c4
lazlanrafar <lazlanrafar@gmail.com>
2022-07-27 22:55:27 +0700

    finish products

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,41 @@
 var express = require("express");
 var router = express.Router();
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 /* GET home page. */
-router.get("/products", function (req, res, next) {
+router.get("/products", async (req, res, next) => {
+  try {
+    const products = await prisma.product.findMany({
+      include: {
+        category: true,
+      },
+    });
+    const categories = await prisma.category.findMany({
+      include: {
+        products: true,
+      },
+    });
+    res.json({ products, categories });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/products/:id", async (req, res, next) => {
   res.render("index", { title: "Express" });
 });
 
-router.get("/products/:id", function (req, res, next) {
+router.post("/products", async (req, res, next) => {
   res.render("index", { title: "Express" });
 });
 
-router.post("/products", function (req, res, next) {
+router.patch("/products/:id", async (req, res, next) => {
   res.render("index", { title: "Express" });
 });
 
-router.patch("/products/:id", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
-
-router.delete("/products/:id", function (req, res, next) {
+router.delete("/products/:id", async (req, res, next) => {
   res.render("index", { title: "Express" });
 });
 
